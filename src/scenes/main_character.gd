89f71e5af3706cabad2ae4ef04991b7bef99dbce
badarouzia <ziane.badarou@epitech.eu>
2024-05-18 16:47:18 +0200

    add the main character mouvement

--- src/scenes/main_character.gd
+++ src/scenes/main_character.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+@onready var sprite_2d = $Sprite2D
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -12,6 +14,7 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		sprite_2d.animation = "jump"
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -26,3 +29,7 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	var isLeft = velocity.x < 0 
+	sprite_2d.flip_h = isLeft 
+ 
+
