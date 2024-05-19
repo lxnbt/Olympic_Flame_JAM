2ca92861879d3a4dcce3cb26868cab459425a808
Loup Thomas <loup.thomas@epitech.eu>
2024-05-19 12:16:49 +0200

    Add player mouvement

--- src/scenes/main_character.gd
+++ src/scenes/main_character.gd
@@ -4,32 +4,14 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 @onready var sprite_2d = $Sprite2D
+var screen_size # Size of the game window.
 
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func _ready():
+	screen_size = get_viewport_rect().size
 
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-		sprite_2d.animation = "jump"
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
-	var isLeft = velocity.x < 0 
-	sprite_2d.flip_h = isLeft 
- 
-
+func _process(delta):
+	position += Input.get_vector("move_left", "move_right", "move_up", "move_down") * SPEED * delta
